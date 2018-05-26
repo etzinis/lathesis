30dacd5b623f0f49807d466e7213110b005f0c00
Efthymios Tzinis <etzinis@gmail.com>
2018-05-27 01:29:46 +0300

    Removing spaces for ref and cite commands as well as translated inline dollar maths

--- lathesis/translation/line_translator.py
+++ lathesis/translation/line_translator.py
@@ -7,6 +7,7 @@
 from googletrans import Translator
 import re
 import argparse
+import numpy as np
 
 
 def get_args():
@@ -25,6 +26,21 @@ def get_args():
     return args
 
 
+def remove_spaces_for_references_and_citations(line):
+
+    refined_line = line
+    for command in ['ref', 'cite']:
+        command_string = "\\"+command+"{"
+        ref_inlines = refined_line.split(command_string)
+        if len(ref_inlines) > 1:
+            for i in np.arange(1, len(ref_inlines)):
+                closure_ind = ref_inlines[i].index('}')
+                ref_inlines[i] = (ref_inlines[i][:closure_ind].replace(
+                                ' ', '') + ref_inlines[i][closure_ind:])
+            refined_line = command_string.join(ref_inlines)
+    return refined_line
+
+
 def refine_latex_translation(new_line):
     refined_line = new_line
     ref_commands = ['ref', 'cite', 'footnote', 'label', 'section',
@@ -32,9 +48,17 @@ def refine_latex_translation(new_line):
     for com in ref_commands:
         refined_line = re.sub("\\ "+com+" {", com+"{", refined_line)
 
-    # p = re.compile("\$[.+\$")
-    # a = p.match(refined_line)
-    refined_line = re.findall("\$.+\$", refined_line)
+    # supress all produced spaces for inline math code, references
+    # and citations
+
+    math_inline = refined_line.split("$")
+    for i in np.arange(1, len(math_inline)-1, 2):
+        math_inline[i] = math_inline[i].replace(' ', '')
+    refined_line = '$'.join(math_inline)
+
+    refined_line = remove_spaces_for_references_and_citations(
+                   refined_line)
+
     return refined_line
 
 
@@ -53,6 +77,7 @@ def translate_line(text, language):
     refined_text = refine_latex_translation(translated_text)
 
     print '\n'
+    print '~'*20
     print refined_text
 
 if __name__ == "__main__":
